e140be455d3c5e4b85cc7eb2c4a7b1e265350d5c
Jakub <jakub014@seznam.cz>
2020-02-10 17:20:40 +0100

    added stripe payments and updated title and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100; //conversion to cents
+  const publisableKey = "pk_test_C9XYwmQCyMsbPuKVCnmB0ESn00FlSGKUuq";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWM Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publisableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
