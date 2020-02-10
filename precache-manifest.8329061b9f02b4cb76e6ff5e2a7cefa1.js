507b1f63b1729b6aac553196a91401d55d28e3fc
Jakub <jakub014@seznam.cz>
2020-02-10 17:26:09 +0100

    Updates

---
+++ precache-manifest.8329061b9f02b4cb76e6ff5e2a7cefa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a7c2f459138ac35825b0ab027d5146",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "7710d45359117a003897",
+    "url": "/crwn-clothing/static/css/main.33c92e13.chunk.css"
+  },
+  {
+    "revision": "125a3801612230d72d1c",
+    "url": "/crwn-clothing/static/js/2.547e9313.chunk.js"
+  },
+  {
+    "revision": "7710d45359117a003897",
+    "url": "/crwn-clothing/static/js/main.9c941ce8.chunk.js"
+  },
+  {
+    "revision": "c6ff65db4cbf77ead457",
+    "url": "/crwn-clothing/static/js/runtime-main.e22ca938.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
