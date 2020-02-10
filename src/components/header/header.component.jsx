f5dd6543a2c859afaef9a6614dd1b1060c27fe97
Jakub <jakub014@seznam.cz>
2020-02-10 18:07:29 +0100

    cleaned unused scripts and dependencies

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -22,9 +22,6 @@ const Header = ({ currentUser, hidden }) => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/contact">
-        Contact
-      </Link>
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
